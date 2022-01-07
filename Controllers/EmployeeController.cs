218509da157a16e8b7f4652aba0a66e9d3c3461f
Ole-Magnus Vian Norum <ole99@live.no>
2022-01-07 10:01:07 +0100

    Add EmployeeController

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace kurs_sommerstudenter_2022.Controllers
+{
+    public class Employee 
+    {
+        public string Name {get; set;}
+        public int Age {get; set;}
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private static List<Employee> _employees = new List<Employee>()
+        {
+            new Employee
+            {
+                Name = "Ole-Magnus Vian Norum",
+                Age = 22
+            },
+
+            new Employee
+            {
+                Name = "Marius Jensen",
+                Age = 26
+            }
+        };
+
+        [HttpGet]
+        public List<Employee> ReadEmployee()
+        {
+            return _employees;
+        }
+    }
+}
